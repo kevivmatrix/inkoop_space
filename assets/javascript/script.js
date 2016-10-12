c481069d3d49734cce62f61edd1612a78dd89ac8
Vivek <vivekpatna52@gmail.com>
2016-10-12 19:51:09 +0530

    dynamic widht height

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,8 +2,12 @@ function init() {
   var stage = new createjs.Stage("demoCanvas");
   var index = 0;
   var width = $(window).width();
-  // alert(width);
-  while (index < 600) {
+  var height = $(window).height();
+  $("#demoCanvas").attr({
+    width: width,
+    height: height
+  });
+  while (index < 800) {
     var circle = new createjs.Shape();
     if (index % 3 == 0) {
       var size = 1;
@@ -11,8 +15,8 @@ function init() {
       var size = 0.5;
     }
     circle.graphics.beginFill("#ffffff").drawCircle(0, 0, size);
-    circle.x = Math.floor((Math.random() * 1600) + 1);
-    circle.y = Math.floor((Math.random() * 1000) + 1);
+    circle.x = Math.floor((Math.random() * width) + 1);
+    circle.y = Math.floor((Math.random() * height) + 1);
     stage.addChild(circle);
     index++;
   }
