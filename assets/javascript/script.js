0fe0180b3b252d18f7cd880f0fae16892e2c26de
Vivek <vivekpatna52@gmail.com>
2016-10-13 14:13:52 +0530

    Yay! mera pyara rocket udd raha hai

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -38,4 +38,23 @@ function init() {
   $("#explore").css({
     marginLeft: -(screen_width*3/10)
   });
+
+  $("#explore_button").on("click", function(e){
+    $("#explore").fadeOut(400);
+  });
+  
+  var rocket_x = -100, rocket_y = -100, velocity = 500;
+  var time = 0;
+  function rocket_fly() {
+    var timer = setTimeout(function(){
+      var new_rocket_x = Math.floor(Math.random() * width);
+      var new_rocket_y = Math.floor(Math.random() * height);
+      var distance_x = new_rocket_x - rocket_x;
+      var distance_y = new_rocket_y - rocket_y;
+      var distance = Math.sqrt( Math.pow(distance_x, 2) + Math.pow(distance_y, 2) );
+      time = Math.floor((distance / velocity) * 1000) ;
+      $("#rocket").animate({ left: new_rocket_x, top: new_rocket_y }, time, rocket_fly);
+    }, 1000);
+  }
+  rocket_fly();
 }
